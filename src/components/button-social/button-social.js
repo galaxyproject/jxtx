c7d1b4035a289c43dde95f7b4ef9e8e6290885ca
Fran McDade <franmcdade@Frans-MBP.hub>
2021-05-18 12:36:10 +1000

    Implement site footer. Includes minor css refactor to site header. #13.

---
+++ src/components/button-social/button-social.js
@@ -0,0 +1,38 @@
+/*
+ * JXTX Foundation
+ * https://www.jxtxfoundation.org
+ *
+ * JXTX button social component.
+ */
+
+// Core dependencies
+import React from "react";
+
+// App dependencies
+import {ButtonClassname} from "../button/button-classname.model";
+import {ButtonSize} from "../button/button-size.model";
+import {ButtonType} from "../button/button-type.model";
+import {Relationship} from "../../utils/anchor/relationship.model";
+import {Target} from "../../utils/anchor/target.model";
+
+// Styles
+import * as buttonStyles from "../button/button.module.css";
+const classNames = require("classnames");
+
+function ButtonSocial(props) {
+
+    const {attributeHREF, children, iconSize} = props;
+    const classButtonSize = ButtonClassname[`${ButtonType.SOCIAL}_${ButtonSize[iconSize]}`];
+    const classButtonType = ButtonClassname[ButtonType.SOCIAL];
+    const classNamesButton = classNames(buttonStyles[classButtonType], buttonStyles[classButtonSize]);
+
+    return (
+        <a className={classNamesButton}
+           href={attributeHREF}
+           rel={Relationship.NOOPENER_NOREFERRER}
+           target={Target.BLANK}>{children}
+        </a>
+    )
+}
+
+export default ButtonSocial;
--- src/components/button/button-classname.model.js
+++ src/components/button/button-classname.model.js
@@ -8,6 +8,8 @@
 export const ButtonClassname = {
     "OUTLINED": "button___outlined",
     "PRIMARY": "button___primary",
+    "SOCIAL": "button__social",
+    "SOCIAL_32": "button__social___oversized",
     "UNELEVATED": "button___unelevated",
     "WHITE": "button___white"
 };
