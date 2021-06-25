fffeb1e4dd07145f188dbae82256f51a09478b17
Fran McDade <franmcdade@Frans-MBP.modem>
2021-06-25 15:11:36 +1000

    Update home page content. Component ButtonCta and ArticleLink internal link checker updated. Home page content updated. #33.

--- src/components/article-typography/article-link.js
+++ src/components/article-typography/article-link.js
@@ -9,22 +9,19 @@
 import Link from "gatsby-link";
 import React from "react";
 
-// App dependencies
-import {Target} from "../../utils/anchor/target.model";
-
 // Styles
 import * as compStyles from "./article-link.module.css";
 
 function ArticleLink(props) {
 
-    const {children, href, target} = props;
-    const externalLink = Target.BLANK === target;
+    const {children, href} = props;
+    const internal = /^\/(?!\/)/.test(href); /* See https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-link/#reminder-use-link-only-for-internal-links. */
 
     return (
         children ?
-            externalLink ?
-            <a className={compStyles.article__link} {...props}>{children}</a> :
-            <Link className={compStyles.article__link} to={href}>{children}</Link> :
+            internal ?
+                <Link className={compStyles.article__link} to={href}>{children}</Link> :
+                <a className={compStyles.article__link} {...props}>{children}</a> :
             null
     )
 }
