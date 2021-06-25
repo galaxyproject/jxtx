290c91d23268b796383112372edc6cec33c5a6bf
Fran McDade <franmcdade@Frans-MBP.modem>
2021-06-25 12:38:04 +1000

    Add news section to home page. #19.

---
+++ src/hooks/newsroom-query.js
@@ -0,0 +1,26 @@
+import {useStaticQuery, graphql} from 'gatsby';
+
+export const NewsroomStaticQuery = () => {
+    const {mdx} = useStaticQuery(
+        graphql`
+        query NewsroomStaticQuery {
+          mdx(fields: {slug: {eq: "/news"}}) {
+            body
+            frontmatter {
+              description
+              fullWidth
+              images {
+                childImageSharp {
+                  gatsbyImageData
+                }
+              }
+              links
+              title
+            }
+            slug
+          }
+        }
+    `
+    );
+    return mdx;
+};
