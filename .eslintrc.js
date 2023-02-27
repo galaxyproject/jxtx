70cddb8ce0169109d488f4dee8d53c8316cf37bd
Dannon Baker <dannon.baker@gmail.com>
2023-02-27 13:04:26 -0500

    Tweak mdx linting options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,27 @@
 module.exports = {
-    extends: ["plugin:mdx/recommended"],
-    // optional, if you want to lint code blocks at the same time
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: ["eslint:recommended", "plugin:react/recommended", "plugin:mdx/recommended"],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {},
     settings: {
         "mdx/code-blocks": true,
-        // optional, if you want to disable language mapper, set it to `false`
-        // if you want to override the default language mapper inside, you can provide your own
         "mdx/language-mapper": {},
     },
-    rules: {
-        // your rules
-        "react/jsx-no-undef": "off",
-    }
+    overrides: [
+        {
+            files: ["*.mdx"],
+            rules: {
+                "react/jsx-no-undef": "off",
+                "no-undef": "warn"
+            },
+        }
+    ],
 };
